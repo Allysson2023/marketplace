481f91dbf13fd053afb95aee267835e5c5a866f3
Allysson2023 <allyssoncarlos.ac21@gmail.com>
2026-05-07 20:39:08 -0300

    Reajuste dos produto da promoção, os produto quando e adicionado, sera os primeiro a aparece na tela

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -5,19 +5,14 @@ const authMiddleware = require('../middlewares/authMiddleware');
 const { route } = require('./userRoutes');
 
 router.post('/products', authMiddleware, (req, res) => {
-
     const { nome, preco, categoria } = req.body;
-
     const userId = req.user.id;
-
     const buscarLoja = "SELECT id FROM stores WHERE user_id = ?";
 
     db.query(buscarLoja, [userId], (err, lojaResult) => {
-
         if (err) {
             return res.status(500).json(err);
         }
-
         if (lojaResult.length === 0) {
             return res.status(404).json({
                 message: "Loja não encontrada"
@@ -25,41 +20,43 @@ router.post('/products', authMiddleware, (req, res) => {
         }
 
         const store_id = lojaResult[0].id;
-
         const sql = `
             INSERT INTO products
             (nome, preco, store_id, categoria)
             VALUES (?, ?, ?, ?)
         `;
-
         db.query(
             sql,
             [nome, preco, store_id, categoria],
             (err, result) => {
-
                 if (err) {
                     return res.status(500).json(err);
                 }
-
                 res.json({
                     message: "Produto criado com sucesso!"
                 });
-
             }
         );
-
     });
-
 });
 
 router.get('/products', (req, res) => {
-    db.query("SELECT * FROM products", (err, result) => {
+
+    const sql = `
+        SELECT * FROM products
+        ORDER BY id DESC
+    `;
+
+    db.query(sql, (err, result) => {
+
         if (err) {
             return res.status(500).json(err);
         }
-        
+
         res.json(result);
+
     });
+
 });
 
 module.exports = router;
