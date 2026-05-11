81943455bcee7210d037e8d6d22610dbfd002d88
Allysson2023 <allyssoncarlos.ac21@gmail.com>
2026-05-11 11:20:59 -0300

    Reajustando o blug id dos produtos da pagina.

--- frontend/frontend/src/App.jsx
+++ frontend/frontend/src/App.jsx
@@ -14,7 +14,7 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/produto/:id" element={<ProductDetalhes />} />
+        <Route path="/product/:id" element={<ProductDetalhes />} />
         <Route path="/login" element={<Login />} />
         <Route path="/carrinho" element={<Carrinho />} />
         <Route path="/cadastrar-produto" element={<CadastrarProduto/>} />
